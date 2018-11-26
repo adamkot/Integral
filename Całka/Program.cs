2db1160d50f79fd50dc5dd2c4f271fc02d6578af
Adam Kotus <a.kotus@gmail.com>
2018-11-26 19:01:46 +0100

    commit no2

--- Całka/Program.cs
+++ Całka/Program.cs
@@ -14,27 +14,82 @@ namespace Całka
         }
 
         private static void mainProgram() {
-            double epsilon;
+            double epsilon = 0;
             double dokladnosc;
             double[] zakres = new double[2];
+            double wynik;
+            int wybranaFunkcja;
 
             Console.WriteLine("Liczenie całki");
             //tu dodać wybór funkcji
-            Console.WriteLine("podaj epsilon: ");
-            epsilon = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("liczba kroków w pierwszym liczeniu: ");
-            dokladnosc = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("zakres od: ");
-            zakres[0] = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("zakres do: ");
-            zakres[1] = Convert.ToDouble(Console.ReadLine());
+            wybranaFunkcja = wybierzFunkcje();
+            epsilon = wczytaj("Podaj epsilon: ", 0.01);
+            dokladnosc = wczytaj("liczba kroków w pierwszym liczeniu: ", 4);
+            zakres[0] = wczytaj("zakres od: " , -1);
+            zakres[1] = wczytaj("zakres do: ", 1);
 
-            Console.WriteLine("Wynik to = " + liczCaleke(epsilon, dokladnosc, zakres));
+            wynik = liczCaleke(epsilon, dokladnosc, zakres, wybranaFunkcja);
+            Console.WriteLine("Wynik to = " + wynik);
             Console.Read();
 
         }
 
-        private static double liczCaleke(double epsilon, double dokladnosc, double[] zakres) {
+        private static int wybierzFunkcje() {
+            int wybor = 0;
+            do
+            {
+                Console.WriteLine("Wybierz funkcje do liczenia: " +
+                "\n1-funkcja kwadratowa" +
+                "\n2-funkcja sinus" +
+                "\n3-funkcja cosinus");
+                string wyb = Console.ReadLine();
+                if (wyb != "")
+                {
+                    try
+                    {
+                        wybor = Convert.ToInt16(wyb);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("podana wartość nie jest liczbą");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Podaj liczbe");
+                }
+            }
+            while (wybor == 0);
+            return wybor;
+        }
+
+        private static double wczytaj(string text, double wartoscDomyslna) {
+            double wynik = 0;
+            do
+            {
+                Console.WriteLine(text);
+                string eps = Console.ReadLine();
+                if (eps != "")
+                {
+                    try
+                    {
+                        wynik = Convert.ToDouble(eps);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("podana wartość nie jest liczbą");
+                    }
+                }
+                else
+                {
+                    wynik = wartoscDomyslna;
+                }
+            }
+            while (wynik == 0);
+            return wynik;
+        }
+
+        private static double liczCaleke(double epsilon, double dokladnosc, double[] zakres, int numerFunkcji) {
             double[] pole = new double[2];
             do
             {
@@ -44,7 +99,7 @@ namespace Całka
                 {
                     for (double kroki = 0; (zakres[0] + kroki) < zakres[1]; kroki += (zakres[1] - zakres[0]) / dokladnosc)
                     {
-                        pole[i] = pole[i] + (double)(Math.Abs(funkcjaKwadratowa(zakres[0] + kroki))* (zakres[1] - zakres[0]) / dokladnosc);
+                        pole[i] = pole[i] + (double)(Math.Abs(wybranaFunkcja(numerFunkcji, zakres[0] + kroki))* (zakres[1] - zakres[0]) / dokladnosc);
                     }
                     dokladnosc = dokladnosc * 2;
                 }
@@ -53,22 +108,18 @@ namespace Całka
             return pole[1];
         }
 
-        private static double funkcjaKwadratowa(double parametr)
+        private static double wybranaFunkcja(int funkcja, double parametr)
         {
-            // funkcja x^2
-            return (parametr * parametr);
-        }
-
-        private static double sinus(double parametr)
-        {
-            // funkcja sin
-            return (Math.Sin(parametr));
-        }
-
-        private static double cos(double parametr)
-        {
-            // funkcja cos
-            return (Math.Cos(parametr));
+            switch (funkcja) {
+                case (1):
+                    return (parametr * parametr);
+                case (2):
+                    return (Math.Sin(parametr));
+                case (3):
+                    return (Math.Cos(parametr));
+                default:
+                    return 0;
+            }
         }
     }
 }
